b1e5e25b9ff2d5d543acbbdc3f6b1dab418cde30
Rafael Palomar <rafael.palomar@rr-research.no>
2014-04-22 14:17:33 +0200

    Initial commit

---
+++ main.cxx
@@ -0,0 +1,38 @@
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkActor.h>
+#include <vtkImplicitPlaneWidget.h>
+
+int main(int argc, char *argv[])
+{
+
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+
+  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  interactor->SetRenderWindow(renderWindow);
+
+  vtkSmartPointer<vtkImplicitPlaneWidget> planeWidget =
+    vtkSmartPointer<vtkImplicitPlaneWidget>::New();
+  planeWidget->SetInteractor(interactor);
+  planeWidget->PlaceWidget(sphereSource->GetOutput()->GetBounds());
+  planeWidget->OutlineTranslationOff();
+  planeWidget->ScaleEnabledOff();
+  planeWidget->OriginTranslationOff();
+  planeWidget->OutsideBoundsOn();
+
+  interactor->Initialize();
+  renderWindow->Render();
+  interactor->Start();
+
+  return EXIT_SUCCESS;
+}
